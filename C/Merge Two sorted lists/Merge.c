5318355af15ad3b1f619bc124f4f50b6553885f4
ZeNeXOP <146000355+ZeNeXOP@users.noreply.github.com>
2024-12-29 22:24:17 +0530

    main

---
+++ C/Merge Two sorted lists/Merge.c
@@ -0,0 +1,35 @@
+
+    
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+ 
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    struct ListNode* temp1 = list1;
+    struct ListNode* temp2 = list2;
+    struct ListNode* temp3 = NULL;
+    struct ListNode* head = list1;
+
+    if (list1 == NULL) return list2;
+    if (list2 == NULL) return list1;
+
+    if (list1->val > list2->val) {
+        temp1 = list2;
+        temp2 = list1;
+        head = list2;
+    }
+
+
+    while (temp1 != NULL && temp2 != NULL){
+            if (temp1->next == NULL || temp2->val <= temp1->next->val){
+                temp3 = temp2->next;
+                temp2->next = temp1->next;
+                temp1->next = temp2;
+                temp2 = temp3;
+            }
+            temp1 = temp1->next;
+    }
+
+    return head;
+}
